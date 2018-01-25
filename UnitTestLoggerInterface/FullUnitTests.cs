f2bf2ddcffd13ef0884768b5be47fa6e1a5e9379
Алексей Козлов <kozlov.as@alliance-jsc.ru>
2018-01-25 14:53:22 +0300

    Добавили функциональность в JHelp теперь есть возможность передачи объекта по интерфейсу не зная его тип

--- UnitTestLoggerInterface/FullUnitTests.cs
+++ UnitTestLoggerInterface/FullUnitTests.cs
@@ -1,4 +1,6 @@
+using KellermanSoftware.CompareNetObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
 using System.IO;
 using WGSTS.Logger;
 
@@ -7,8 +9,8 @@ namespace UnitTestLoggers
     [TestClass]
     public class FullUnitTests
     {
-     
-      
+
+
 
         [TestMethod]
         public void IntefaceFromJsonJHelp()
@@ -45,6 +47,91 @@ namespace UnitTestLoggers
 
             Assert.AreEqual(str, @"""sdjhasdkjasdhjkas""");
 
+        }
+
+
+        class Data
+        {
+            public IEnumerable Val { get; set; }
+        }
+
+
+        interface ITestData
+        {
+            int Test { get; set; }
+        }
+
+        class Data1 : ITestData
+        {
+            public int Test { get; set; }
+
+            public string TestString { get; set; }
+        }
+
+
+        class Data2 : ITestData
+        {
+            public int Test { get; set; }
+
+            public decimal TestDecimal { get; set; }
+        }
+
+
+
+        [TestMethod]
+        public void IntefaceToJsonAutoJHelpT()
+        {
+            var data = new Data() { Val = new byte[] { 1, 2, 3 } };
+
+            //            File.WriteAllText(@"c:\temp\data1.json", inf.ToJsonAuto());
+
+            Assert.AreEqual(data.ToJsonAuto(), @"{""Val"":{""$type"":""System.Byte[], System.Private.CoreLib"",""$value"":""AQID""}}");
+
+            ITestData str = (ITestData)(new Data1() { Test = 1, TestString = "str" });
+
+            str = (ITestData)(new Data2() { Test = 1, TestDecimal = 0.5M });
+
+            var inf = str.ToJsonAuto().FromJsonAuto<ITestData>();
+            Assert.AreEqual(null, inf);
+
+
+        }
+
+
+        [TestMethod]
+        public void IntefaceFromJsonAutoJHelpT()
+        {
+            var data = new Data() { Val = new byte[] { 1, 2, 3 } };
+            var str = @"{""Val"":{""$type"":""System.Byte[], System.Private.CoreLib"",""$value"":""AQID""}}".FromJsonAuto<Data>();
+            CompareLogic compareLogic = new CompareLogic();
+            ComparisonResult result;
+            result = compareLogic.Compare(data, str);
+            Assert.AreEqual(true, result.AreEqual);
+
+
+            str = (Data)@"{""Val"":{""$type"":""System.Byte[], System.Private.CoreLib"",""$value"":""AQID""}}".FromJsonAuto(typeof(Data));
+            compareLogic = new CompareLogic();
+            
+            result = compareLogic.Compare(data, str);
+            Assert.AreEqual(true, result.AreEqual);
+        }
+
+
+        [TestMethod]
+        public void IntefaceToJsonFromAllJHelpT()
+        {
+            ITestData str = (ITestData)(new Data1() { Test = 1, TestString = "str" });
+            str = (ITestData)(new Data2() { Test = 1, TestDecimal = 0.5M });
+            var inf = str.ToJsonAll().FromJsonAll<ITestData>();
+            CompareLogic compareLogic = new CompareLogic();
+            ComparisonResult result;
+            result = compareLogic.Compare(inf, str);
+            Assert.AreEqual(true, result.AreEqual);
+
+            inf = (ITestData)str.ToJsonAll().FromJsonAll(typeof(ITestData));
+            compareLogic = new CompareLogic();
+            result = compareLogic.Compare(inf, str);
+            Assert.AreEqual(true, result.AreEqual);
         }
 
         [TestMethod]
--- WGSTS.LoggerInterfase/JHelp.cs
+++ WGSTS.LoggerInterfase/JHelp.cs
@@ -32,6 +32,76 @@ namespace WGSTS.Logger
             }
         }
 
+
+        public static object FromJsonAuto(this string value, Type tp)
+        {
+            var settings = new JsonSerializerSettings()
+            {              
+                TypeNameHandling = TypeNameHandling.Auto
+            };
+            try
+            {
+                return JsonConvert.DeserializeObject(value, tp, settings);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+        public static T FromJsonAuto<T>(this string value)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.Auto
+            };
+
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(value, settings);
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
+
+        public static object FromJsonAll(this string value, Type tp)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.All
+            };
+            try
+            {
+                return JsonConvert.DeserializeObject(value, tp, settings);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+        public static T FromJsonAll<T>(this string value)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.All
+            };
+
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(value, settings);
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
         public static string ToJson(this object value)
         {
             return ToJson(value, false);
@@ -43,6 +113,52 @@ namespace WGSTS.Logger
             var settings = new JsonSerializerSettings()
             {
                 ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+            try
+            {
+                return JsonConvert.SerializeObject(value, formating ? Formatting.Indented : Formatting.None, settings);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static string ToJsonAuto(this object value)
+        {
+            return ToJsonAuto(value, false);
+        }
+
+
+        public static string ToJsonAuto(this object value, bool formating)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling =  ReferenceLoopHandling.Ignore,
+                TypeNameHandling = TypeNameHandling.Auto                
+            };
+            try
+            {
+                return JsonConvert.SerializeObject(value, formating ? Formatting.Indented : Formatting.None, settings);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static string ToJsonAll(this object value)
+        {
+            return ToJsonAll(value, false);
+        }
+
+
+        public static string ToJsonAll(this object value, bool formating)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                TypeNameHandling = TypeNameHandling.All
             };
             try
             {
